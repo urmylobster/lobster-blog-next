482604c63964e7bf27d567a4b5f8675eaa84274b
imurlobster <alexander2720@163.com>
2023-12-25 17:56:36 +0800

    mod: next framework initialization

--- src/auto-import.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '@/services/post'
-declare module '@/plugins/axios'
-declare module '@/utils/businessUtils'
-declare module 'umi'
--- src/services/post.ts
+++
@@ -1,8 +0,0 @@
-import request from '@/plugins/axios';
-
-export function getPostsList(){
-  return request({
-    url: '/posts/list',
-    method: 'GET',
-  })
-}
